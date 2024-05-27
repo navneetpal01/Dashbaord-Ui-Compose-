5023fa434dfcb006c485d4350ebb3382d54426f1
navneet <navneetpalgithub@gmail.com>
2024-05-27 19:33:47 +0530

    Saturn

--- app/src/main/java/com/example/dashboard_ui_compose/DashboardScreen.kt
+++ app/src/main/java/com/example/dashboard_ui_compose/DashboardScreen.kt
@@ -11,16 +11,24 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,7 +48,7 @@ fun DashboardScreen() {
     ) {
         ConstraintLayout {
             //To create multiple variables from a single value
-            val (topImg, profile,button) = createRefs()
+            val (topImg, profile, button) = createRefs()
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -110,20 +118,105 @@ fun DashboardScreen() {
                         start.linkTo(parent.start)
                         end.linkTo(parent.end)
                     }
-            ){
+            ) {
 
                 Column(
                     modifier = Modifier
                         .padding(top = 12.dp, bottom = 12.dp, end = 12.dp)
                         .height(90.dp)
-                ){
-
+                        .width(90.dp)
+                        .background(
+                            color = Color(android.graphics.Color.parseColor("#b6c2fe")),
+                            shape = RoundedCornerShape(20.dp)
+                        ),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.video_call),
+                        contentDescription = "Video Call",
+                        modifier = Modifier
+                            .size(65.dp)
+                            .padding(top = 8.dp, bottom = 4.dp)
+                    )
+                    Text(
+                        text = "Video Call",
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontStyle = FontStyle.Italic,
+                        color = Color(android.graphics.Color.parseColor("#5e3bee"))
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .padding(top = 12.dp, bottom = 12.dp, end = 8.dp)
+                        .height(90.dp)
+                        .width(90.dp)
+                        .background(
+                            color = Color(android.graphics.Color.parseColor("#b6c2fe")),
+                            shape = RoundedCornerShape(20.dp)
+                        ),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.notification),
+                        contentDescription = "Notification",
+                        modifier = Modifier
+                            .size(65.dp)
+                            .padding(top = 8.dp, bottom = 4.dp)
+                    )
+                    Text(
+                        text = "Notification",
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontStyle = FontStyle.Italic,
+                        color = Color(android.graphics.Color.parseColor("#5e3bee"))
+                    )
+                }
+                Column(
+                    modifier = Modifier
+                        .padding(top = 12.dp, bottom = 12.dp, start = 8.dp)
+                        .height(90.dp)
+                        .width(90.dp)
+                        .background(
+                            color = Color(android.graphics.Color.parseColor("#b6c2fe")),
+                            shape = RoundedCornerShape(20.dp)
+                        ),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.voice_call),
+                        contentDescription = "Voice Call",
+                        modifier = Modifier
+                            .size(65.dp)
+                            .padding(top = 8.dp, bottom = 4.dp)
+                    )
+                    Text(
+                        text = "Voice Call",
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontStyle = FontStyle.Italic,
+                        color = Color(android.graphics.Color.parseColor("#5e3bee"))
+                    )
                 }
 
             }
 
-
         }
+        var text by rememberSaveable { mutableStateOf("") }
+        TextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            label = {
+                Text(
+                    text = "Searching for..."
+                )
+            },
+            trailingIcon = {
+                Image(painter = painterResource(id = R.drawable.search_icon), contentDescription = null)
+            }
+        )
     }
 }
 
